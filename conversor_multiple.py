0aa1d09c88535a67e4441621a65edf48d3799cc0
Denniels <34431737+Denniels@users.noreply.github.com>
2022-10-11 02:37:53 -0300

    Cambios

--- conversor_multiple.py
+++ conversor_multiple.py
@@ -11,23 +11,33 @@ Escoje una opcion
 opcion = int(input(menu))
 
 def conversor_dolar(peso_pais, dolar):
+        '''
+        Funcion conversor_dolar(peso_pais, dolar):
+        Args:
+                peso_pais: Valor del peso de un pais ejemplo peso chileno
+                dolar: Valor del dolar en el peso del pais
+                return peso_pais / dolar
+        '''
         return peso_pais / dolar
+
 # Peso Colombiano a dolar
 if opcion == 1:
         pesos_colombianos = int(input('Cuantos pesos Colombianos tienes?: '))
-        print(f'Tienes {conversor_dolar(pesos_colombianos, 0.00022)} dolares')
+        print(f'Tienes {round(conversor_dolar(pesos_colombianos, 4606.03), 2)} dolares')
 
 # Peso Argentino a dolar
-if opcion == 2:
+elif opcion == 2:
         pesos_argentinos = int(input('Cuantos pesos Argentinos tienes?: '))
-        print(f'Tienes {conversor_dolar(pesos_argentinos, 0.0067)} dolares')
+        print(f'Tienes {round(conversor_dolar(pesos_argentinos, 149.19), 2)} dolares')
 
 # Peso Chileno a dolar
-if opcion == 3:
+elif opcion == 3:
         pesos_chilenos = int(input('Cuantos pesos Chilenos tienes?: '))
         print(f'Tienes {round(conversor_dolar(pesos_chilenos, 938.18), 2)} dolares')
 
 # Peso Mexicano a dolar
-if opcion == 4:
+elif opcion == 4:
         pesos_mexicanos = int(input('Cuantos pesos Mexicanos tienes?: '))
-        print(f'Tienes {conversor_dolar(pesos_mexicanos, 0.050)} dolares')
+        print(f'Tienes {round(conversor_dolar(pesos_mexicanos, 20.00), 2)} dolares')
+else:
+        print('Opcion incorrecta, por favor escoje una opcion valida')
